ec6b6c8a143f2e96a5238ca7fc8ba084beefacf6
potato865 <724895582@qq.com>
2020-05-30 19:01:58 +0800

    添加主页header组件

---
+++ src/views/home/components/Header.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="header">
+    <div class="headerLeft">
+      <div class="iconfont back-icon">&#xe624;</div>
+    </div>
+    <div class="headerInput">
+      <span class="iconfont">&#xe632;</span>
+      输入城市/景点/游玩主题
+    </div>
+    <div class="headerRight">
+      城市
+      <span class="iconfont arrow-icon">&#xe600;</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomeHeader"
+};
+</script>
+
+<style lang="scss" scoped>
+@import "styles/varibles.scss";
+.header {
+  line-height: 0.86rem;
+  display: flex;
+  background: $bgColor;
+  color: #fff;
+  .headerLeft {
+    width: 0.64rem;
+    float: left;
+    .back-icon {
+      font-size: 0.4rem;
+      text-align: center;
+    }
+  }
+  .headerInput {
+    flex: 1;
+    background: #fff;
+    height: 0.64rem;
+    line-height: 0.64rem;
+    margin-top: 0.12rem;
+    margin-left: 0.2rem;
+    padding-left: 0.2rem;
+    border-radius: 0.1rem;
+    color: #ccc;
+  }
+  .headerRight {
+    width: 1.04rem;
+    float: right;
+    text-align: center;
+    padding: 0 0.1rem;
+    .arrow-icon {
+      margin-left: -0.04rem;
+      font-size: 0.24rem;
+    }
+  }
+}
+</style>
---
+++ vue.config.js
@@ -0,0 +1,12 @@
+const path = require('path');
+function resolve (dir) {
+    return path.join(__dirname, dir)
+}
+module.exports = {
+    lintOnSave: true,
+    chainWebpack: (config)=>{
+        config.resolve.alias
+            .set('styles',resolve('src/assets/styles'))
+            .set('components',resolve('src/components'))
+    }
+}
