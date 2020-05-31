1ab5a0624e2a5bf33fe9ff27ae93fc3cb21e5769
potato865 <724895582@qq.com>
2020-05-31 22:11:55 +0800

    修改生产环境url

--- src/utils/request.js
+++ src/utils/request.js
@@ -2,8 +2,8 @@ import Axios from "axios";
 
 const baseURL =
   process.env.NODE_ENV === "development"
-    ? "http://localhost:8080/api/travel/"
-    : "http://www.potato865.cn/travel/static/mock";
+    ? "http://www.potato865.cn/api/travel"
+    : "http://localhost:8080/api/travel/";
 const instance = Axios.create({
   baseURL,
   timeout: 5000
