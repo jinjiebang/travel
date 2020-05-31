fbf4d1ed51a8743ef9d13d96f68a2a79c54aa78f
potato865 <724895582@qq.com>
2020-05-31 22:19:12 +0800

    修复一个bug

--- src/utils/request.js
+++ src/utils/request.js
@@ -2,8 +2,8 @@ import Axios from "axios";
 
 const baseURL =
   process.env.NODE_ENV === "development"
-    ? "http://www.potato865.cn/travel/static/mock/"
-    : "http://localhost:8080/api/travel/";
+    ? "http://localhost:8080/api/travel/"
+    : "http://www.potato865.cn/travel/static/mock/";
 const instance = Axios.create({
   baseURL,
   timeout: 5000
