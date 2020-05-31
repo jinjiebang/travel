605447b45fb8609b63d19f3b9aac38972e258848
potato865 <724895582@qq.com>
2020-05-31 15:51:17 +0800

    热销推荐组件开发

--- src/views/home/components/Icons.vue
+++ src/views/home/components/Icons.vue
@@ -103,34 +103,37 @@ export default {
 </style>
 <style lang="scss" scoped>
 @import "styles/varibles.scss";
-.icon {
-  float: left;
-  width: 25%;
-  height: 0;
-  padding-bottom: 25%;
-  position: relative;
-  .icon-img {
-    position: absolute;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0.44rem;
-    text-align: center;
-    .icon-img-content {
-      height: 100%;
+.icons {
+  margin-top: 0.1rem;
+  .icon {
+    float: left;
+    width: 25%;
+    height: 0;
+    padding-bottom: 25%;
+    position: relative;
+    .icon-img {
+      position: absolute;
+      top: 0;
+      left: 0;
+      right: 0;
+      bottom: 0.44rem;
+      text-align: center;
+      .icon-img-content {
+        height: 100%;
+      }
     }
-  }
-  .icon-desc {
-    position: absolute;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    height: 0.44rem;
-    line-height: 0.44rem;
-    text-align: center;
-    margin: 0;
-    color: $darkTextColor;
-    @include ellipsis;
+    .icon-desc {
+      position: absolute;
+      left: 0;
+      right: 0;
+      bottom: 0;
+      height: 0.44rem;
+      line-height: 0.44rem;
+      text-align: center;
+      margin: 0;
+      color: $darkTextColor;
+      @include ellipsis;
+    }
   }
 }
 </style>
---
+++ src/views/home/components/Recommend.vue
@@ -0,0 +1,95 @@
+<template>
+  <div>
+    <div class="recommend-title">热销推荐</div>
+    <ul>
+      <li class="item" v-for="item of recommendList" :key="item.id">
+        <div class="item-img-wrapper">
+          <img :src="item.imgUrl" class="item-img" />
+        </div>
+        <div class="item-info">
+          <p class="item-title">{{ item.title }}</p>
+          <p class="item-desc">{{ item.desc }}</p>
+          <button class="item-button">查看详情</button>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomeRecommend",
+  data() {
+    return {
+      recommendList: [
+        {
+          id: "001",
+          imgUrl:
+            "http://img1.qunarzz.com/sight/p0/2005/a9/a9411416ff215fe2a3.water.jpg_200x200_3110fb7b.jpg",
+          title: "上海迪士尼乐园",
+          desc: "全球最大的迪士尼城堡"
+        },
+        {
+          id: "001",
+          imgUrl:
+            "http://img1.qunarzz.com/sight/p0/2005/a9/a9411416ff215fe2a3.water.jpg_200x200_3110fb7b.jpg",
+          title: "上海迪士尼乐园",
+          desc: "全球最大的迪士尼城堡"
+        },
+        {
+          id: "001",
+          imgUrl:
+            "http://img1.qunarzz.com/sight/p0/2005/a9/a9411416ff215fe2a3.water.jpg_200x200_3110fb7b.jpg",
+          title: "上海迪士尼乐园",
+          desc: "全球最大的迪士尼城堡"
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+@import "styles/varibles.scss";
+.recommend-title {
+  margin-top: 0.2rem;
+  line-height: 0.8rem;
+  background: #eee;
+  text-indent: 0.2rem;
+}
+.item {
+  overflow: hidden;
+  display: flex;
+  height: 1.9rem;
+  .item-img {
+    width: 1.7rem;
+    height: 1.7rem;
+    padding: 0.1rem;
+  }
+  .item-info {
+    flex: 1;
+    padding: 0.1rem;
+    min-width: 0;
+    .item-title {
+      line-height: 0.54rem;
+      font-size: 0.32rem;
+      @include ellipsis;
+    }
+    .item-desc {
+      color: #ccc;
+      line-height: 0.4rem;
+      @include ellipsis;
+    }
+    .item-button {
+      line-height: 0.44rem;
+      margin-top: 0.16rem;
+      background: #ff9300;
+      padding: 0 0.2rem;
+      border-radius: 0.06rem;
+      border: 1px solid transparent;
+      color: #fff;
+      outline: none;
+    }
+  }
+}
+</style>
