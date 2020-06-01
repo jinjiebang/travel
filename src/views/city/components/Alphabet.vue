50e60e2daf81343d6f65b257e634547de70b3829
potato865 <724895582@qq.com>
2020-06-01 23:32:57 +0800

    字母表组件支持拖动

--- src/views/city/components/Alphabet.vue
+++ src/views/city/components/Alphabet.vue
@@ -1,12 +1,15 @@
 <template>
-  <ul class="list">
+  <ul class="list" ref="current">
     <li
       class="item"
-      v-for="(item, key) of cities"
-      :key="key"
+      v-for="item of letters"
+      :key="item"
+      :ref="item"
       @click="handleClickLetter"
+      @touchstart="handleTouchStart"
+      @touchmove="handleTouchMove"
     >
-      {{ key }}
+      {{ item }}
     </li>
   </ul>
 </template>
@@ -16,9 +19,45 @@ export default {
   props: {
     cities: Object
   },
+  data() {
+    return {
+      touchStatus: false,
+      startY: 0,
+      itemHeight: 0,
+      offsetTop: 0,
+      timer: 0
+    };
+  },
+  updated() {
+    this.startY = this.$refs["A"][0].offsetTop;
+    this.itemHeight = this.$refs["A"][0].offsetHeight;
+    this.offsetTop = this.$refs["current"].offsetTop;
+  },
+  computed: {
+    letters() {
+      return Object.keys(this.cities);
+    }
+  },
   methods: {
     handleClickLetter(e) {
       this.$emit("change", e.target.innerText);
+    },
+    handleTouchStart() {
+      this.touchStatus = true;
+    },
+    handleTouchMove(e) {
+      if (this.touchStatus) {
+        if (this.timer) {
+          clearTimeout(this.timer);
+        }
+        setTimeout(() => {
+          const touchY = e.touches[0].clientY - this.offsetTop;
+          const index = Math.floor((touchY - this.startY) / this.itemHeight);
+          if (index > 0 && index < this.letters.length) {
+            this.$emit("change", this.letters[index]);
+          }
+        }, 16);
+      }
     }
   }
 };
