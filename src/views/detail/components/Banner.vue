f0ef24ff701eea54203fb351a686ca0aeb9d85df
potato865 <724895582@qq.com>
2020-06-05 23:44:40 +0800

    新增画廊组件

--- src/views/detail/components/Banner.vue
+++ src/views/detail/components/Banner.vue
@@ -1,23 +1,51 @@
 <template>
-  <div class="banner">
-    <div class="banner-img">
-      <img
-        src="//img1.qunarzz.com/sight/p0/2005/82/8253605ce47e06eba3.water.jpg_600x330_dd0e3a16.jpg"
-        alt
-      />
-    </div>
-    <div class="banner-info">
-      <div class="banner-title">上海海昌海洋公园</div>
-      <div class="banner-number">
-        <span class="iconfont banner-icon">&#xe616;</span>102
+  <div>
+    <div class="banner" @click="handleBannerClick">
+      <div class="banner-img">
+        <img
+          src="//img1.qunarzz.com/sight/p0/2005/82/8253605ce47e06eba3.water.jpg_600x330_dd0e3a16.jpg"
+          alt
+        />
+      </div>
+      <div class="banner-info">
+        <div class="banner-title">上海海昌海洋公园</div>
+        <div class="banner-number">
+          <span class="iconfont banner-icon">&#xe616;</span>102
+        </div>
       </div>
     </div>
+    <gallery
+      :imgs="imgs"
+      v-show="showGallary"
+      @close="handleGallaryClose"
+    ></gallery>
   </div>
 </template>
 
 <script>
+import Gallery from "@/components/gallery";
 export default {
-  name: "DetailBanner"
+  name: "DetailBanner",
+  data() {
+    return {
+      imgs: [
+        "http://img1.qunarzz.com/sight/p0/2005/82/8253605ce47e06eba3.water.jpg_r_800x800_4f0c207c.jpg",
+        "http://img1.qunarzz.com/sight/p0/2003/7d/7d9779fadf1a9750a3.water.jpg_r_800x800_71fe9069.jpg"
+      ],
+      showGallary: false
+    };
+  },
+  methods: {
+    handleBannerClick() {
+      this.showGallary = true;
+    },
+    handleGallaryClose() {
+      this.showGallary = false;
+    }
+  },
+  components: {
+    Gallery
+  }
 };
 </script>
 
